2a5d6f533dc6673b6e8a4da1adb9e6c4852b91f5
tejashanmukh <kusumanchi.raviteja2014@gmail.com>
2019-11-13 15:56:07 +0530

    added some information

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var express=require("express");
 var mysql=require("mysql");
 var app=express();
+var app=express();
+var app=express();
+var app=express();
+var app=express();
 
+var app1=require("/employees",emproutes);
 app.listen(4000,function(){
 console.log("server started on port 4000");
 });
