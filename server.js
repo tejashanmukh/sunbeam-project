35a2f3fa99f31637d8678231005e893f8046dbd8
tejashanmukh <kusumanchi.raviteja2014@gmail.com>
2019-11-14 22:48:14 +0530

    changes made by teja

--- server.js
+++ server.js
@@ -3,14 +3,15 @@ var mysql=require("mysql");
 var mysql=require("mysql");
 var mysql=require("mysql");
 var mysql=require("mysql");
+iiikeeddsaaa:
 var app=express();
-var app=express();
-var app=express();
-var app=express();
-var app=express();
-var app=express();var app=express();var app=express();var app=express();
+var app1=require("/employees",emproutes);
+app.listen(4000,function(){
+console.log("server started on port 4000");
+});qqqiilllnkfbdsbbdskbanalkbn
 
 var app1=require("/employees",emproutes);
 app.listen(4000,function(){
 console.log("server started on port 4000");
-});
+});qqqiilllnkfbdsbbdskbanalkbn
+i am raviteja in cdac
